3f18ed7564a351f84c8c1bedf9d01d9e54f25d76
ravi <rmanshahia@gmail.com>
2018-10-24 18:36:48 +0530

    S11L146 : Showing or Hiding Elements based on User's role.

--- src/app/helpers/fake-backend.ts
+++ src/app/helpers/fake-backend.ts
@@ -5,7 +5,7 @@ export function fakeBackendFactory(
     backend: MockBackend, 
     options: BaseRequestOptions) {
         
-  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik1vc2ggSGFtZWRhbmkiLCJhZG1pbiI6dHJ1ZX0.iy8az1ZDe-_hS8GLDKsQKgPHvWpHl0zkQBqy1QIPOkA';
+  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.dyt0CoTl4WoVjAHI9Q_CwSKhl6d_9rhM3NrXuJttkao';
     
   backend.connections.subscribe((connection: MockConnection) => {
     // We are using the setTimeout() function to simulate an 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -28,5 +28,42 @@ logout() {
 isLoggedIn() {
   return tokenNotExpired();
 }
+
+get currentUser() {
+
+  let token = localStorage.getItem('token');
+  if(!token) 
+    return null;
+
+  return new JwtHelper().decodeToken(token);
+}
+   //Here the value would be EQUAL to the payload of the token sent from the server.
+    //eg here the structure is 
+    //
+    // {
+    //   "sub": "1234567890",
+    //   "name": "John Doe",
+    //   "admin": false
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
