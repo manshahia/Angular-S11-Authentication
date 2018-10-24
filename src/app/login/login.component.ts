2b794e2802d56cdb89dacc3233cf2ec9e24f60ea
ravi <rmanshahia@gmail.com>
2018-10-24 16:17:35 +0530

    Authentication and Authorization : LOGIN Impementation.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,15 @@ import { Router } from "@angular/router";
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+//NOTES for LOGIN
+  // So, to recap, when the user logs in, you should send a post request to an API endpoint to validate their credentials.
+  // If the credentials are valid, we should get a json web token, and store it in the local storage, and then its better
+  // to return a boolean instead of a response object.Finally, in our LOGIN COMPONENT, we get that boolean, and depending
+  // on its value, we either navigate the user to a different page which is often the homepage, or display a validation error.
+  // This is how we implement the login.
+
+ //ALSO in this project we are using Obsolete Angular project and jwt library, however the above algorithm is still valid. 
 export class LoginComponent {
   invalidLogin: boolean; 
 
