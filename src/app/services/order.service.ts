c8286e325107ff01c1f574e38972afd4a1fc8e59
ravi <rmanshahia@gmail.com>
2018-10-24 19:21:25 +0530

    S11L151: Accessing Protected API roles.

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,15 +1,27 @@
-import { Http } from '@angular/http';
+import { Http, RequestOptions } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
+import { AuthHttp } from 'angular2-jwt';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private http: Http) {
+  //Using AuthHttp has same interface has standard Http, and using it we don't need to write code to include headers,
+  //it manages it all by itself. And we can comment the code below.
+  constructor(private http: AuthHttp) {
   }
 
   getOrders() { 
     return this.http.get('/api/orders')
       .map(response => response.json());
+
+      // let headers = new Headers();
+      // let token = localStorage.getItem('token');
+      // headers.append('Authorization', 'Bearer ' + token);
+
+      // let options = new RequestOptions({ headers: headers});
+
+      // return this.http.get('/api/orders', options)
+      //   .map(response => response.json());
   }
 }
